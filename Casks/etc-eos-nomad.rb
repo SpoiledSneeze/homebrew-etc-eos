d429304a943560392a0bad31e00602c5c3715e0b
SpoiledSneeze <53020446+SpoiledSneeze@users.noreply.github.com>
2025-11-02 19:48:57 -0500

    fix:caveats
    fix: caveats

--- Casks/etc-eos-nomad.rb
+++ Casks/etc-eos-nomad.rb
@@ -1,6 +1,6 @@
 cask "etc-eos-nomad" do
   version "3.2.12"
-  sha256 :no_check # ETC doesn't provide checksums; verify manually after download
+  sha256 :no_check
 
   url "https://www.etcconnect.com/WorkArea/DownloadAsset.aspx?id=10737506112"
   name "ETC Eos Family (ETCnomad)"
@@ -10,6 +10,7 @@ cask "etc-eos-nomad" do
   livecheck do
     url "https://support.etcconnect.com/ETC/Consoles/Eos_Family/Software_and_Programming/All_Eos_Family_Software_Versions"
     regex(/^(\d+\.\d+\.\d+)\s*\|/)
+  end
 
   pkg "ETCnomad_Eos_Mac_v#{version}.pkg"
 
@@ -26,19 +27,21 @@ cask "etc-eos-nomad" do
     "~/Library/Saved Application State/com.etc.eos.*",
   ]
 
-  caveats <<~EOS
-    ETCnomad requires a USB hardware key (dongle) to function beyond demo mode.
-    
-    After installation, launch the application from:
-      /Applications/Eos Family/Eos Configuration Utility
-    
-    Documentation and keyboard shortcuts can be found at:
-      ~/Documents/ETC/Eos Family Documents/
-    
-    For the latest version information and downloads, visit:
-      https://www.etcconnect.com/eos-software/
-    
-    Note: This cask installs Eos v3.x series. For legacy v2.9 support
-    (required for older XP-based consoles), see 'etc-eos-nomad@2.9'
-  EOS
+  caveats do
+    <<~EOS
+      ETCnomad requires a USB hardware key (dongle) to function beyond demo mode.
+
+      After installation, launch the application from:
+        /Applications/Eos Family/Eos Configuration Utility
+
+      Documentation and keyboard shortcuts can be found at:
+        ~/Documents/ETC/Eos Family Documents/
+
+      For the latest version information and downloads, visit:
+        https://www.etcconnect.com/eos-software/
+
+      Note: This cask installs Eos v3.x series. For legacy v2.9 support
+      (required for older XP-based consoles), see 'etc-eos-nomad@2.9'
+    EOS
+  end
 end
