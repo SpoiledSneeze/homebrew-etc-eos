63d7b5a1fb16c06e050cf5900faeee90c4fbbe2f
SpoiledSneeze <53020446+SpoiledSneeze@users.noreply.github.com>
2025-11-02 19:43:34 -0500

    fix:url stanza

--- Casks/etc-eos-nomad.rb
+++ Casks/etc-eos-nomad.rb
@@ -2,7 +2,7 @@ cask "etc-eos-nomad" do
   version "3.2.12"
   sha256 :no_check # ETC doesn't provide checksums; verify manually after download
 
-  url "https://www.etcconnect.com/WorkArea/DownloadAsset.aspx?id=10737506112",
+  url "https://www.etcconnect.com/WorkArea/DownloadAsset.aspx?id=10737506112"
   name "ETC Eos Family (ETCnomad)"
   desc "Lighting control software for theatrical and architectural applications"
   homepage "https://www.etcconnect.com/Products/Consoles/Eos-Family/ETCnomad-ETCnomad-Puck/"
